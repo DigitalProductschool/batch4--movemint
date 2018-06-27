96917798a5e648e09db52bbe91ad8b0d0315e9c8
Robin <robin.stoehr@code.berlin>
2018-06-27 10:51:06 +0200

    Added Buttons

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, Button} from 'react-native';
 
 class GeolocationExample extends Component {
     constructor(props) {
@@ -12,8 +12,8 @@ class GeolocationExample extends Component {
         };
     }
 
-    componentDidMount() {
-        navigator.geolocation.watchPosition(
+    startTracking = () => {
+         this.watchId = navigator.geolocation.watchPosition(
             (position) => {
                 this.setState({
                     latitude: position.coords.latitude,
@@ -21,17 +21,26 @@ class GeolocationExample extends Component {
                     error: null,
                 });
             },
-            (error) => this.setState({ error: error.message }),
-            { enableHighAccuracy: false, timeout: 20000, maximumAge: 1000, distanceFilter: 1 },
+            (error) => this.setState({error: error.message}),
+            {enableHighAccuracy: false, timeout: 20000, maximumAge: 1000, distanceFilter: 1},
         );
+    }
+
+    stopTracking = () => {
+        navigator.geolocation.clearWatch(this.watchId);
+        navigator.geolocation.stopObserving();
     }
 
     render() {
         return (
-            <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Latitude: {this.state.latitude}</Text>
-                <Text>Longitude: {this.state.longitude}</Text>
-                {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+            <View>
+                <Button title="Start tracking" onPress={this.startTracking}/>
+                <View style={{flexGrow: 1, alignItems: 'center', justifyContent: 'center'}}>
+                    <Text>Latitude: {this.state.latitude}</Text>
+                    <Text>Longitude: {this.state.longitude}</Text>
+                    {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+                </View>
+                <Button title="Stop tracking" onPress={this.stopTracking} color="red"/>
             </View>
         );
     }
