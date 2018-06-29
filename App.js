cf6fbad7fab8acb579370db34452900fb2758a40
Parth <vermaparth97@gmail.com>
2018-06-29 13:02:43 +0200

    Removed unused imports, added Robin's path to local.properties

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
-
 import Backgroundtracker from './BackgroudTracker';
 
 export default Backgroundtracker;
