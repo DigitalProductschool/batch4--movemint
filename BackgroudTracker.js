648526c7a6363cbb58793d46c36e948e06c49288
Robin <robin.stoehr@code.berlin>
2018-07-05 13:22:24 +0200

    Start and stop track functions

--- BackgroudTracker.js
+++ BackgroudTracker.js
@@ -14,6 +14,8 @@ import BackgroundGeolocation from "react-native-mauron85-background-geolocation"
 import renderIf from "./renderIf";
 import BackgroundTimer from "react-native-background-timer";
 import GifComponent from "./src/components/GifComponent/GifComponent"
+import { startTrack } from "./src/utilities/startTrack";
+import { stopTrack } from "./src/utilities/stopTrack";
 
 const { UIManager } = NativeModules;
 
@@ -82,135 +84,11 @@ class BgTracking extends Component {
     };
   }
 
-  onToggle(isOn) {
-    alert("Changed to " + isOn);
-  }
 
-  startTrack = () => {
-    //Alert.alert("Tracking Started!");
-    BackgroundGeolocation.configure({
-      desiredAccuracy: BackgroundGeolocation.HIGH_ACCURACY,
-      stationaryRadius: 50,
-      distanceFilter: 50,
-      notificationTitle: "Background tracking",
-      notificationText: "enabled",
-      debug: true,
-      startOnBoot: false,
-      stopOnTerminate: false,
-      locationProvider: BackgroundGeolocation.ACTIVITY_PROVIDER,
-      interval: 10000,
-      fastestInterval: 5000,
-      activitiesInterval: 10000,
-      stopOnStillActivity: false,
-      url: "http://192.168.81.15:3000/location",
-      httpHeaders: {
-        "X-FOO": "bar"
-      },
-      // customize post properties
-      postTemplate: {
-        lat: "@latitude",
-        lon: "@longitude"
-      }
-    });
 
-    BackgroundGeolocation.on("location", location => {
-      // handle your locations here
-      // to perform long running operation on iOS
-      // you need to create background task
-      console.log(location);
-      BackgroundGeolocation.startTask(taskKey => {
-        // execute long running task
-        // eg. ajax post location
-        // IMPORTANT: task has to be ended by endTask
-        //console.log("");
-        BackgroundGeolocation.endTask(taskKey);
-      });
-    });
-
-    BackgroundGeolocation.on("stationary", stationaryLocation => {
-      // handle stationary locations here
-      console.log("haha", stationaryLocation);
-      Actions.sendLocation(stationaryLocation);
-    });
-
-    BackgroundGeolocation.on("error", error => {
-      console.log("[ERROR] BackgroundGeolocation error:", error);
-    });
-
-    BackgroundGeolocation.on("start", () => {
-      console.log("[INFO] BackgroundGeolocation service has been stasrted");
-    });
-
-    BackgroundGeolocation.on("stop", () => {
-      console.log("[INFO] BackgroundGeolocation service has been stopped");
-    });
-
-    BackgroundGeolocation.on("authorization", status => {
-      console.log(
-        "[INFO] BackgroundGeolocation authorization status: " + status
-      );
-      if (status !== BackgroundGeolocation.AUTHORIZED) {
-        // we need to set delay or otherwise alert may not be shown
-        setTimeout(
-          () =>
-            Alert.alert(
-              "App requires location tracking permission",
-              "Would you like to open app settings?",
-              [
-                {
-                  text: "Yes",
-                  onPress: () => BackgroundGeolocation.showAppSettings()
-                },
-                {
-                  text: "No",
-                  onPress: () => console.log("No Pressed"),
-                  style: "cancel"
-                }
-              ]
-            ),
-          1000
-        );
-      }
-    });
-
-    BackgroundGeolocation.on("background", () => {
-      console.log("[INFO] App is in background");
-      console.log(location);
-    });
-
-    BackgroundGeolocation.on("foreground", () => {
-      console.log("[INFO] App is in foreground");
-    });
-
-    BackgroundGeolocation.checkStatus(status => {
-      console.log(
-        "[INFO] BackgroundGeolocation service is running",
-        status.isRunning
-      );
-      console.log(
-        "[INFO] BackgroundGeolocation services enabled",
-        status.locationServicesEnabled
-      );
-      console.log(
-        "[INFO] BackgroundGeolocation auth status: " + status.authorization
-      );
-    });
-
-    // you can also just start without checking for status
-    BackgroundGeolocation.start();
-    //BackgroundGeolocation.background();
-  };
-
-  stopTrack = () => {
-    // unregister all event listeners
-    BackgroundGeolocation.stop();
-    BackgroundGeolocation.events.forEach(event =>
-      BackgroundGeolocation.removeAllListeners(event)
-    );
-  };
 
   startTrackingFunction() {
-    this.startTrack();
+    startTrack();
 
     // Animate the update
     LayoutAnimation.spring();
@@ -231,7 +109,7 @@ class BgTracking extends Component {
   }
 
   stopTrackingFunciton() {
-    this.stopTrack();
+    stopTrack();
 
     LayoutAnimation.spring();
     this.setState({
