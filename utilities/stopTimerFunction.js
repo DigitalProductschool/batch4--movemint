30fd29a8586f95f1cc620b7e4f96f27a4336cc00
Parth <vermaparth97@gmail.com>
2018-07-09 10:56:28 +0200

    Unnecessary files deleted

--- utilities/stopTimerFunction.js
+++
@@ -1,6 +0,0 @@
-import BackgroundTimer from "react-native-background-timer";
-
-export function stopTimerFunction() {
-    console.log("Timer Stopped!");
-    BackgroundTimer.clearInterval(this.intervalId);
-}
--- utilities/stopTrack.js
+++
@@ -1,9 +0,0 @@
-import BackgroundGeolocation from "react-native-mauron85-background-geolocation";
-
-export const stopTrack = () => {
-    // unregister all event listeners
-    BackgroundGeolocation.stop();
-    BackgroundGeolocation.events.forEach(event =>
-        BackgroundGeolocation.removeAllListeners(event)
-    );
-};
