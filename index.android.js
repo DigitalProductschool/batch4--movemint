947ce6718fe309e6c643a78117397855f0d1af98
Parth <vermaparth97@gmail.com>
2018-07-19 13:19:32 +0200

    Changed file to sync user

--- index.android.js
+++ index.android.js
@@ -1,6 +1,36 @@
 import React, { Component } from 'react';
 import { Alert, View, Button, Text, StyleSheet } from 'react-native';
-import realm from './src/utilities/realm'
+//import realm from './src/utilities/realm'
+import Realm from 'realm';
+
+var username = 'parth';
+var password = 'parth';
+var server = 'https://movemintserver.de1a.cloud.realm.io/';
+
+class Tripsdb extends Realm.Object { }
+Tripsdb.schema = {
+    name: 'Trips',
+    //primaryKey: 'tripID',
+    properties: {
+        tripID: { type: 'int' },
+        lat: { type: 'list', objectType: 'string' },
+        lon: { type: 'list', objectType: 'string' },
+        timestamp: { type: 'list', objectType: 'string' }
+    }
+};
+
+class Usersdb extends Realm.Object { }
+Usersdb.schema = {
+    name: 'Users',
+    //primaryKey: 'userID',
+    properties: {
+        userName: { type: 'string' },
+        userID: { type: 'int' },
+        totalDistance: { type: 'int' },
+        totalTrips: { type: 'int' },
+        trips: { type: 'list', objectType: 'Trips' }
+    }
+};
 
 class Database extends Component {
   constructor(props) {
@@ -8,25 +38,37 @@ class Database extends Component {
   }
 
   gottheLocation() {
-    realm.write(() => {
-      realm.create('Users',
-        {
-          userName: 'Parth',
-          userID: 1,
-          totalDistance: 10,
-          totalTrips: 1,
-          trips: [{
-            tripID: 2,
-            lat: this.props.geoStates.lat,
-            lon: this.props.geoStates.lon,
-            timestamp: this.props.geoStates.timestamp,
-          }],
+    Realm.Sync.User.login(server, username, password, (error, user) => {
+      if (!error) {
+        Realm.open({
+          sync: {
+            user: user,
+            url: 'https://movemintserver.de1a.cloud.realm.io/~/locationdata',
+          },
+          schema: [Tripsdb, Usersdb]
+        }).then(realm => {
+          realm.write(() => {
+            realm.create('Users',
+              {
+                userName: user,
+                userID: 1,
+                totalDistance: 10,
+                totalTrips: 1,
+                trips: [{
+                  tripID: 2,
+                  lat: this.props.geoStates.lat,
+                  lon: this.props.geoStates.lon,
+                  timestamp: this.props.geoStates.timestamp,
+                }],
+              });
+          });
+          let allusers = realm.objects('Users');
+          console.log('users');
+          console.log(JSON.stringify(allusers))
         });
+      }
     });
-    let allusers = realm.objects('Users');
-    console.log('users');
-    console.log(JSON.stringify(allusers))
-  }
+}
 
   deleteAllUsers() {
     realm.write(() => {
@@ -44,15 +86,7 @@ class Database extends Component {
     if(this.props.screenState=="Home")
         this.gottheLocation();
   }
-  
-  // componentDidUpdate() {
-  //   console.log('Component Did Update')
-  //   if (realm.objects('Users').length > 6) { this.deleteAllUsers(); }
 
-  //   console.log(this.props.screenState)
-  //   if(this.props.screenState=="Home")
-  //       this.gottheLocation();
-  // }
   render() {
     return (
       <View>
