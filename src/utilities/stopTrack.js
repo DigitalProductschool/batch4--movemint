31dd81260602bbdad83b695882e84fc325c6836c
Parth <vermaparth97@gmail.com>
2018-07-21 14:18:34 +0200

    remove console log statements

--- src/utilities/stopTrack.js
+++ src/utilities/stopTrack.js
@@ -7,10 +7,6 @@ function pushToDatabase(lat, lon, time) {
   const totaltrips = realm.objects("Trips").length;
 
   realm.write(() => {
-    console.log("Values in Push to Database")
-    console.log("Lat Array: " + lat);
-    console.log("Lon Array: " + lon);
-    console.log("Time Array: " + time);
 
     const newTrip = realm.create("Trips", {
       tripID: totaltrips + 1,
@@ -26,11 +22,6 @@ function pushToDatabase(lat, lon, time) {
 }
 
 export const stopTrack = (lat, lon, time) => {
-  // unregister all event listeners
-  console.log("Values in Stop Track")
-  console.log("Lat Array: " + lat);
-  console.log("Lon Array: " + lon);
-  console.log("Time Array: " + time);
   pushToDatabase(lat, lon, time);
 
   BackgroundGeolocation.stop();
